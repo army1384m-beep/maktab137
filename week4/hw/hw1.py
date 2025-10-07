a603d356913aff0eb667efb7141f69d8ead23c27
maryam <army1384m@gmail.com>
2025-10-07 22:03:10 +0330

    Add week4 folder structure

--- week4/hw/hw1.py
+++ week4/hw/hw1.py
@@ -1,21 +1,15 @@
-from datetime import datetime
-
-def time_restriction(func):
-    def wrapper(*args, **kwargs):
-        current_time = datetime.now()
-        hour = current_time.hour
-        
-        if 8 <= hour < 17:
-            return func(*args, **kwargs)
-        else:
-            return "Error: Running outside allowed hours"
-    
-    return wrapper
+def time_restriction(start_hour=8, end_hour=17):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            current_hour = datetime.now().hour
+            if start_hour <= current_hour < end_hour:
+                return func(*args, **kwargs)
+            else:
+                return f"Error: Running outside allowed hours ({start_hour}:00 - {end_hour}:00)"
+        return wrapper
+    return decorator
 
 
-
-@time_restriction
+@time_restriction(9, 18)  
 def my_function():
     return "Function executed."
-
-print(my_function())
